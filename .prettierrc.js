fc4648c0e3c60846483e376a67c8ba38d52cf873
1cedrus <aowiej23s@gmail.com>
2024-04-30 13:53:14 +0700

    Install deps, setup ts, prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+const config= {
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    jsxSingleQuote: true,
+    printWidth: 120,
+    tabWidth: 2,
+    bracketSameLine: true,
+};
+
+export default config;
