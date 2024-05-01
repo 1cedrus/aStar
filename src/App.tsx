38de13315c59f334236e2a3807faacca64af987d
1cedrus <aowiej23s@gmail.com>
2024-05-01 14:52:03 +0700

    Add github link 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, IconButton } from '@chakra-ui/react';
 import Puzzle from '@/components/Puzzle.tsx';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { GithubIcon } from '@/icons.tsx';
 
 function App() {
   return (
     <Flex h='100vh' w='full' justify='center' align='center'>
       <Puzzle />
       <ToastContainer />
+      <Box as='form' action='https://github.com/1cedrus/aStar' position='absolute' right='1rem' bottom='1rem'>
+        <IconButton type='submit' aria-label={'Github'} icon={<GithubIcon />} variant='outline' />
+      </Box>
     </Flex>
   );
 }
